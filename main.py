12fc33edc67f02015712b1653e85ed590146fcf1
Neolog13 <neolog13@gmail.com>
2023-06-06 14:42:15 +0530

    zero commit. problem db

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy(app)
+
+
+# DB - Table - Notes
+# id    title    price    isActive
+# 1     Some     100      True
+# 2     Some2    200      False
+# 3     Some3    40       True
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    # text = db.Column(db.Text, nullable=False)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
